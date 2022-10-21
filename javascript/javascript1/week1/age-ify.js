e06b79ea0e1c30faec05770700c65f7c6d8f2725
na-talia <ntsh_89@ukr.net>
2022-10-21 22:31:53 +0200

    changed let to const

--- javascript/javascript1/week1/age-ify.js
+++ javascript/javascript1/week1/age-ify.js
@@ -1,5 +1,5 @@
 const yearOfBirth = 1989;
 const yearFuture = 2030;
-let age = yearFuture - yearOfBirth;
+const age = yearFuture - yearOfBirth;
 
 console.log(`You will be ${age} years old in ${yearFuture}.`);
