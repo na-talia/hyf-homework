6667d93047810a89a60bdca28bba6d4d54d42b9e
na-talia <ntsh_89@ukr.net>
2022-10-26 19:24:41 +0200

    add flight booking

---
+++ javascript/javascript1/week2/flight-booking.js
@@ -0,0 +1,15 @@
+function getFullName(firstname, surname, useFormalName, gender) {
+  if (useFormalName === true && gender === "male") {
+    return `Lord ${firstname} ${surname}`;
+  } else if (useFormalName === true && gender === "female") {
+    return `Lady ${firstname} ${surname}`;
+  } else {
+    return `${firstname} ${surname}`;
+  }
+}
+
+const fullname1 = getFullName("Benjamin", "Hughes", true, "male");
+const fullname2 = getFullName("Kate", "Jensen", false, "female");
+
+console.log(fullname1);
+console.log(fullname2);
