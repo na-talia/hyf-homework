6dc6baec3b37e762b775cb4a73a6c954e4c60c9f
na-talia <ntsh_89@ukr.net>
2022-12-08 20:48:34 +0100

    add Giphy api

---
+++ javascript/javascript3/week1/giphy-api.js
@@ -0,0 +1,25 @@
+const gifKey = "t2bqf6dFtQjkVxvGZj9ldvsQ2Q0k1Gjp";
+const divContainer = document.getElementById("container");
+
+async function getGifs(word, howMany) {
+  // Visual Studio Code helped me to convert my function to async function, it works, so I decided to leave it like that
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/search?q=${word}}&limit=${howMany}&api_key=${gifKey}`
+  );
+  return await response.json();
+}
+
+document.getElementById("gif-button").addEventListener("click", () => {
+  divContainer.innerHTML = "";
+
+  const gifName = document.getElementById("gif-name").value;
+  const gifNumber = document.getElementById("gif-numbers").value;
+
+  getGifs(gifName, gifNumber).then((gif) => {
+    gif.data.forEach((element) => {
+      const gifImg = document.createElement("iframe");
+      gifImg.setAttribute("src", element.embed_url);
+      divContainer.appendChild(gifImg);
+    });
+  });
+});
