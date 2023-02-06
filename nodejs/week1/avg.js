46149f2d81c414c19d5c334cfc9aa453b93b673f
na-talia <ntsh_89@ukr.net>
2023-02-06 20:01:46 +0100

    change const name

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -2,15 +2,15 @@
 The first item (argv[0]) will be the path to node itself, and the second item (argv[1]) will be the path to your script code.
 So a slice starting at 2 will discard both of those and return everything else that was typed on the command line.*/
 
-const arguments = process.argv.slice(2);
+const getArguments = process.argv.slice(2);
 
-const sum = arguments.reduce((prev, curr) => Number(prev) + Number(curr), 0);
+const sum = getArguments.reduce((prev, curr) => Number(prev) + Number(curr), 0);
 
-if (arguments.length === 0) {
+if (getArguments.length === 0) {
   console.log("No arguments are provided");
 } else if (isNaN(sum)) {
   console.log("Please provide numbers only");
 } else {
-  const average = (sum / arguments.length).toFixed(2);
+  const average = (sum / getArguments.length).toFixed(2);
   console.log(`The average is: ${average}`);
 }
